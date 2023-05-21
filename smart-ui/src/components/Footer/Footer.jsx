ce2759b397109334040966c86c0c0240b1f004dd
TuanPA2002 <phianhtuan31@gmail.com>
2023-05-21 23:48:58 +0700

    UI Footer

--- smart-ui/src/components/Footer/Footer.jsx
+++ smart-ui/src/components/Footer/Footer.jsx
@@ -1,6 +1,65 @@
-import "./Footer.scss";
+import React from 'react';
+import './Footer.scss';
+
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from 'react-icons/fa';
+import Payment from '../../assets/payments.png';
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="col">
+                    <div className="title">About</div>
+                    <div className="text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae repudiandae eveniet dolorem
+                        libero nihil saepe, maiores culpa dolore omnis nisi, pariatur velit voluptatem iure mollitia.
+                        Qui rem excepturi impedit illum!
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Contact</div>
+                    <div className="c-item">
+                        <FaLocationArrow />
+                        <div className="text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae repudiandae eveniet
+                            dolorem libero nihil saepe
+                        </div>
+                    </div>
+                    <div className="c-item">
+                        <FaMobileAlt />
+                        <div className="text">Phone: 0336 526 9311</div>
+                    </div>
+                    <div className="c-item">
+                        <FaEnvelope />
+                        <div className="text">Email: phianhtuan31@gmail.com</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Categories</div>
+                    <span className="text">Sách tiếng anh</span>
+                    <span className="text">Máy tính</span>
+                    <span className="text">Đồ dùng học tập</span>
+                    <span className="text">Điện thoại</span>
+                    <span className="text">Giày dép</span>
+                    <span className="text">Quần áo</span>
+                </div>
+                <div className="col">
+                    <div className="title">Page</div>
+                    <span className="text">home</span>
+                    <span className="text">about</span>
+                    <span className="text">Contact us</span>
+                    <span className="text">facebook</span>
+                    <span className="text">React JS</span>
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <div className="bottom-bar-content">
+                    <div className="text">được tạo bởi TuanPA - Mẫu thiết kế mới nhất.</div>
+                    <img src={Payment} alt="" />
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- smart-ui/src/components/Footer/Newsletter/Newsletter.jsx
+++ smart-ui/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,35 @@
-import "./Newsletter.scss";
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+import './Newsletter.scss';
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+    return (
+        <div className="newsletter-section">
+            <div className="newsletter-content">
+                <span className="big-text">hãy luôn vui vẻ vì cuộc sống còn nhiều điều thú vị hơn </span>
+                <div className="form">
+                    <input type="text" placeholder="Email address" />
+                    <button>Send mail</button>
+                </div>
+                <div className="text">Will be any more...</div>
+                <div className="social-icons">
+                    <div className="icon">
+                        <FaFacebookF size={14} />
+                    </div>
+                    <div className="icon">
+                        <FaTwitter size={14} />
+                    </div>
+                    <div className="icon">
+                        <FaInstagram size={14} />
+                    </div>
+                    <div className="icon">
+                        <FaLinkedinIn size={14} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Newsletter;
